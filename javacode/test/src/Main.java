952d870b8b4f99babd69cffc2523892fcec1d9cd
ynxiyan <yn.xiyan@qq.com>
2023-04-19 10:24:34 +0800

    测试插件库

---
+++ javacode/test/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: XIYAN
+ * @Date: 2023/4/19 10:20
+ * @注释:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
